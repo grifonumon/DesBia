5c548ecb21a74db6444df77847d379c0c09a1b89
GRyFf <robert_spataru_7@yahoo.com>
2019-06-11 13:31:48 +0300

    fix

--- form.php
+++ form.php
@@ -47,16 +47,17 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
 <link href="//db.onlinewebfonts.com/c/a4e256ed67403c6ad5d43937ed48a77b?family=Core+Sans+N+W01+35+Light" rel="stylesheet" type="text/css"/>
 <link rel="stylesheet" href="form.css" type="text/css">
+<link rel="stylesheet" href="StyleMenu.css">
 <div class="body-content">
   <div class="module">
-    <h1>Inregistrare</h1>
+    <h1 style="color: black;"><strong>Inregistrare</strong></h1>
     <form class="form" action="form.php" method="post" enctype="multipart/form-data" autocomplete="off">
       <div class="alert alert-error"><?=$_SESSION['message'] ?></div>
       <input type="text" placeholder="Nume" name="username" required />
       <input type="email" placeholder="Email" name="email" required />
       <input type="password" placeholder="Parola" name="password" autocomplete="new-password" required />
       <input type="password" placeholder="Confirmare Parola" name="confirmpassword" autocomplete="new-password" required />
-      <div class="avatar"><label>Selectare imagine: </label><input type="file" name="avatar" accept="image/*" required /></div>
+      <div class="avatar"><label style="color: black;">Selectare imagine: </label><input type="file" name="avatar" accept="image/*" required /></div>
       <input type="submit" value="Inregistrare" name="register" class="btn btn-block btn-primary" />
     </form>
   </div>
